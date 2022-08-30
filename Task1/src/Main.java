520b23e66d202d979ff6d7cb4644e4752b7e3d2a
Igor <triavr@mail.ru>
2022-08-30 13:43:25 +0300

    Соm2

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -43,9 +43,26 @@ public class Main {
 
         }
 
+        int max = m[0][0];
+        int min2 = m[0][0];
+        int avg = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                if (max < m[i][j])
+                    max = m[i][j];
+                if (min2 > m[i][j])
+                    min2 = m[i][j];
+                avg = avg + m[i][j] / 56;
+            }
+        }
+
+
+
+
 
         System.out.println("Max значение элемента массива = " +counter);
         System.out.println("Min значение элемента массива = " +min);
+        System.out.println("Среднее = " + avg);
     }
 
 }
